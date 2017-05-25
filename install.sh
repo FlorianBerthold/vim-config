db8f4775675190e76bf83706aab2c9dc74395dee
Florian Andrew George Berthold <florian.berthold@sub-net.at>
2017-05-25 21:21:18 +0200

    update wget cmd

--- install.sh
+++ install.sh
@@ -15,12 +15,12 @@ curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
 	
 
 # colors
-wget https://raw.githubusercontent.com/joshdick/onedark.vim/master/colors/onedark.vim -O ~/.vim/colors/onedark.vim
-wget https://raw.githubusercontent.com/joshdick/onedark.vim/master/autoload/onedark.vim -O ~/.vim/autoload/onedark.vim
-wget https://raw.githubusercontent.com/joshdick/onedark.vim/master/autoload/lightline/colorscheme/onedark.vim -O ~/.vim/autoload/lightline/colorscheme/onedark.vim
+wget -qN https://raw.githubusercontent.com/joshdick/onedark.vim/master/colors/onedark.vim -O ~/.vim/colors/onedark.vim
+wget -qN https://raw.githubusercontent.com/joshdick/onedark.vim/master/autoload/onedark.vim -O ~/.vim/autoload/onedark.vim
+wget -qN https://raw.githubusercontent.com/joshdick/onedark.vim/master/autoload/lightline/colorscheme/onedark.vim -O ~/.vim/autoload/lightline/colorscheme/onedark.vim
 
 
 # get the config
-wget https://raw.githubusercontent.com/FlorianBerthold/vim-config/master/.vimrc -O ~/.vimrc
+wget -qN https://raw.githubusercontent.com/FlorianBerthold/vim-config/master/.vimrc -O ~/.vimrc
 
 
