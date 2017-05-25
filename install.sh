750181e5cd71f466378df7c524ec6b01bb22ac22
Florian Andrew George Berthold <florian.berthold@sub-net.at>
2017-05-25 21:14:41 +0200

    fix urls

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+aptitude install vim -y
+
 mkdir -p ~/.vim/colors
 # mkdir -p ~/.vim/autoload
 mkdir -p ~/.vim/autoload/airline/themes
@@ -14,9 +16,10 @@ curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
 
 # colors
 wget https://raw.githubusercontent.com/joshdick/onedark.vim/master/colors/onedark.vim -O ~/.vim/colors/onedark.vim
-wget https://raw.github.com/joshdick/onedark.vim/blob/master/autoload/onedark.vim -O ~/.vim/autoload/onedark.vim
-wget https://raw.githubusercontent.com/joshdick/onedark.vim/master/autoload/airline/themes/onedark.vim -O ~/.vim/autoload/airline/themes/onedark.vim
+wget https://raw.githubusercontent.com/joshdick/onedark.vim/master/autoload/onedark.vim -O ~/.vim/autoload/onedark.vim
+wget https://raw.githubusercontent.com/joshdick/onedark.vim/master/autoload/lightline/colorscheme/onedark.vim -O ~/.vim/autoload/lightline/colorscheme/onedark.vim
 
 
 # get the config
-wget https://raw.githubusercontent.com/FlorianBerthold/vim-config/.vimrc -O ~/.vimrc
+wget https://raw.githubusercontent.com/FlorianBerthold/vim-config/master/.vimrc -O ~/.vimrc
+
