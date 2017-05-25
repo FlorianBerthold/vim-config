2a0d632b337c196c48946dba5617364cd47a4158
Florian Andrew George Berthold <fb@sub-net.at>
2017-05-26 01:06:17 +0200

    cleanup

--- install.sh
+++ install.sh
@@ -5,28 +5,14 @@
 # cleanup
 rm -rf ~/.vim 
 
-# make the dirs we need
-#mkdir -p ~/.vim/colors
-#mkdir -p ~/.vim/autoload
-#mkdir -p ~/.vim/autoload/lightline/colorscheme
-#mkdir -p ~/.vim/autoload/airline/themes
-
-
+# plugin manager Plug
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-# neovim
-# curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
-    # https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-	
-
-# colors
-#wget -qN https://raw.githubusercontent.com/joshdick/onedark.vim/master/colors/onedark.vim -O ~/.vim/colors/onedark.vim
-#wget -qN https://raw.githubusercontent.com/joshdick/onedark.vim/master/autoload/onedark.vim -O ~/.vim/autoload/onedark.vim
-#wget -qN https://raw.githubusercontent.com/joshdick/onedark.vim/master/autoload/airline/themes/onedark.vim -O ~/.vim/autoload/airline/themes/onedark.vim
-#wget -qN https://raw.githubusercontent.com/joshdick/onedark.vim/master/autoload/lightline/colorscheme/onedark.vim -O ~/.vim/autoload/lightline/colorscheme/onedark.vim
+# basics
+curl -fLo ~/.vim/colors/badwolf.vim --create-dirs \
+   https://raw.githubusercontent.com/sjl/badwolf/master/colors/badwolf.vim
 
 # get the config
 wget -qN https://raw.githubusercontent.com/FlorianBerthold/vim-config/master/.vimrc -O ~/.vimrc
-
 
